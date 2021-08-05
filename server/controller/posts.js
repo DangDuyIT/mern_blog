0a16c055d8b5ce3437907d9a4b164000c2acd76f
Dang Duy <anhmatdieuhau@gmail.com>
2021-08-05 21:51:32 +0700

    condig db and post

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,48 @@
-export const getPost = (req, res) => {
-  res.send("Post");
+import { PostModel } from "../models/post_model.js";
+
+export const getPost = async (req, res) => {
+  try {
+    const post = new PostModel({
+      title: "HI",
+      content: "Anhon",
+      author: "Anonymous",
+    });
+
+    post.save();
+
+    const posts = await PostModel.find();
+    console.log("Post", posts);
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+export const createPost = (req, res) => {
+  try {
+    const newPost = req.body;
+
+    const post = new PostModel(newPost);
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+};
+
+export const updatePost = (req, res) => {
+  try {
+    const updatePost = req.body;
+
+    const post = await new PostModel.findOneAndUpdate(
+      { _id: updatePost._id },
+      updatePost,
+      {new: true}
+    );
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
 };
